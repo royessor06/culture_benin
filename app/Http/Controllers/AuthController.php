9ac3b01b879fedd355c190e5208df99971f554b8
Roy SAVY <savyroyessor@gmail.com>
2025-12-16 10:37:37 +0100

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,7 +65,7 @@ class AuthController extends Controller
             'prenom'=>$validated['prenom'],
             'email'=>$validated['email'],
             'mot_de_passe'=>bcrypt($validated['mot_de_passe']),
-            'sexe' => $validated['sexe'] ?? 'non_defini',
+            'sexe' => 'X',
             'role_id'=>$roleAdmin->id,
         ]);
 
