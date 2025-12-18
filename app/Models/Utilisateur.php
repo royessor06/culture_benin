6449f5fcc61a3c65a87105e8691eabe61d5c86a6
Roy SAVY <savyroyessor@gmail.com>
2025-12-18 19:36:50 +0100

    Update Utilisateur.php

--- app/Models/Utilisateur.php
+++ app/Models/Utilisateur.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class Utilisateur extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<\Database\Factories\UtilisateurFactory> */
     use HasFactory, Notifiable;
 
     /**
